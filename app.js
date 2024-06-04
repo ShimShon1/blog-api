824be230752f5b9e181193492010ba0077cf2e50
Dean <deaneffi1414@gmail.com>
2024-06-04 15:10:55 +0300

    add helmet, rate limiter

--- app.js
+++ app.js
@@ -6,13 +6,22 @@ const ProtectedPostsRouter = require("./routes/postsProtected");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const app = express();
-
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 require("dotenv").config();
 
 mongoose.connect(process.env.DB_LINK);
 app.use(express.json());
 app.use(cors());
-
+app.use(helmet());
+app.use(
+  rateLimit({
+    windowMs: 2 * 60 * 1000,
+    limit: 40,
+    standardHeaders: "draft-7",
+    legacyHeaders: false,
+  })
+);
 async function verifyUser(req, res, next) {
   try {
     const token = req.headers.authorization.split(" ")[1];
@@ -31,7 +40,6 @@ app.use("/api", IndexRouter);
 
 app.use(verifyUser);
 app.get("/api/isLogged", (req, res) => {
-  console.log("logged");
   return res.json({ logged: true });
 });
 app.use("/api/protected/posts", ProtectedPostsRouter, verifyUser);
